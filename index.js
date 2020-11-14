e1f78092af39ccc1dc9f2346e778814036286887
Oleg Eliseev <olegeliseev96@gmail.com>
2020-11-15 01:08:49 +0300

    Finished REST api

--- index.js
+++ index.js
@@ -1,19 +1,108 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname +'/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
-//Homepage
+// ============================ MONGO CONNECTION AND SCHEMA ============================
+
+mongoose.set("debug", true);
+mongoose.Promise = Promise; //Позволяет использовать промисы с методами монго
+mongoose.connect("mongodb://localhost:27017/node-todo", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN")
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+const todoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "Это поле не может быть пустым"
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+// ============================ HOMEPAGE ============================
+
 app.get("/", function(req, res) {
     res.sendFile("index.html");
 });
 
-//Api Todos
+// ============================ TODOS API ============================
+
+//All todos
+app.route("/api/todos")
+
+//Get all todos
+.get(function(req, res) {
+    Todo.find()
+        .then(function(todos) {
+            res.json(todos)
+        })
+})
+
+//Create todo
+.post(function(req, res) {
+    Todo.create(req.body)
+        .then(function(todo) {
+            res.json(todo);
+        })
+        .catch(function(err) {
+            res.send(`Something went wrong: ${err}`);
+        })
+})
+
+//Specific todo
+app.route("/api/todos/:todoId")
+
+//Get specific todo
+.get(function(req, res) {
+    Todo.findById({ _id: req.params.todoId })
+        .then(function(todo) {
+            res.json(todo)
+        })
+        .catch(function(err) {
+            res.send(err)
+        })
+})
+
+//Update specific todo
+.put(function(req, res) {
+    Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+    .then(function(todo) {
+        res.json(todo);
+    })
+    .catch(function(err) {
+        res.send(`Something went wrong: ${err}`);
+    })
+})
+
+//Delete specific todo
+.delete(function(req,res) {
+    Todo.deleteOne({ _id: req.params.todoId })
+    .then(function() {
+        res.send("Todo deleted successfully");
+    })
+    .catch(function(err) {
+        res.send(`Something went wrong: ${err}`);
+    })
+})
 
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, () => console.log("SERVER RUNNING"));
